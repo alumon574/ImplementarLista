0aa8dec82d11a03c6d0b6ce515bb26f7131f0e34
alu10522574 <alumon574@ieselcaminas.org>
2022-05-25 19:42:07 +0200

    Lista terminada (falta testear)

--- src/ObjectLinkedList.java
+++ src/ObjectLinkedList.java
@@ -1,33 +1,96 @@
 import java.util.LinkedList;
 
 public class ObjectLinkedList {
+    private Element firstElement = null;
+
     public ObjectLinkedList() {
     }
 
     public void insertFirst(Object obj) {
-
+        Element newElement = new Element(obj);
+        newElement.setNext(firstElement);
+        firstElement = newElement;
     }
 
     public void insertLast(Object obj) {
+        Element lastElement = new Element(obj);
+        Element currentElement = firstElement;
+        if (currentElement != null) {
+            while (currentElement.getNext() != null) {
+                currentElement = currentElement.getNext();
+            }
+            currentElement.setNext(lastElement);
+        }
     }
 
     public void print() {
+        Element currentElement = firstElement;
+        if (currentElement != null) {
+            while (currentElement.getNext() != null) {
+                System.out.println(currentElement);
+                currentElement = currentElement.getNext();
+            }
     }
+        }
 
     public boolean isEmpty() {
-        return false;
+        return firstElement == null;
     }
 
     public void remove(Object obj) throws ObjectNotFoundException, EmptyListException {
+        int indexOfObject = findIndexOf(obj);
+        if (indexOfObject<0) return;
+        Element targetElement=getElementAtPosition(indexOfObject);
+        Element nextElement = targetElement.getNext();
+        if (targetElement==firstElement)
+            firstElement=firstElement.getNext();
+        else{
+            Element previousElement = getElementAtPosition(indexOfObject-1);
+            previousElement.setNext(nextElement);
+        }
+        targetElement.delete();
+    }
+
+    private int findIndexOf(Object obj) {
+        int index=-1;
+        Element currentElement = firstElement;
+        while (currentElement!=null){
+            index++;
+            if (currentElement.getObject()==obj)
+                break;
+            currentElement=currentElement.getNext();
+        }
+        return index;
     }
 
     public Object getFirstObject() throws EmptyListException {
+        return firstElement;
     }
 
     public Object getLastObject() throws EmptyListException {
+        Element currentElement = firstElement;
+        if (currentElement != null) {
+            while (currentElement.getNext() != null) {
+                currentElement = currentElement.getNext();
+            }
+        }
+        return currentElement;
     }
 
     public Object getObjectAtPosition(int i) throws EmptyListException, InvalidIndexException {
+        Element element = getElementAtPosition(i);
+        if (element == null) return null;
+        return element.getObject();
+    }
+
+    private Element getElementAtPosition(int i){
+        int target=0;
+        Element currentElement = firstElement;
+        while (target!=i){
+            currentElement=currentElement.getNext();
+            i++;
+        }
+        return currentElement;
     }
 
     private class Element {
@@ -37,7 +100,6 @@ public class ObjectLinkedList {
 
         public Element(Object newObject) {
             this.object = newObject;
-
         }
 
         public void setNext(Element newNext) {
